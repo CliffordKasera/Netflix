1d5e2ef15a0def314a0c719a5c92d9895799e97c
Blankphrase <ckasera6486@gmail.com>
2018-07-18 18:19:58 +0300

    registering database

--- netflix/settings.py
+++ netflix/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'netflixclone.apps.NetflixcloneConfig'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,10 @@ WSGI_APPLICATION = 'netflix.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'netflix',
+        'USER': 'blankphrase',
+        'PASSWORD': 'password',
     }
 }
 
